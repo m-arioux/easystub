7628e4e028a1bc1c51ba5e82e6cf611b38ba7689
m-arioux <88749079+m-arioux@users.noreply.github.com>
2025-05-16 09:41:05 -0400

    introduced repository

--- EasyStub.API/infrastructure/endpoints.repository.ts
+++ EasyStub.API/infrastructure/endpoints.repository.ts
@@ -1,9 +1,48 @@
-import { Endpoint } from "../Endpoint.model";
+import { Mutex } from "async-mutex";
+import { Endpoint, EndpointId, UnsavedEndpoint } from "../Endpoint.model";
+
+// to be thread-safe
+const mutex = new Mutex();
 
 export class EndpointsRepository {
-  endpoints: Endpoint[] = [];
+  endpoints: Record<EndpointId, Endpoint | undefined> = {};
 
-  constructor() {}
+  public async create(endpoint: UnsavedEndpoint): Promise<Endpoint> {
+    return await mutex.runExclusive(() => {
+      const id = this.getNextId();
 
-  public create(endpoint: Endpoint): void {}
+      const newEndpoint = { ...endpoint, id };
+
+      this.endpoints[id] = newEndpoint;
+
+      return newEndpoint;
+    });
+  }
+
+  private getNextId() {
+    const existingIds = Object.values(this.endpoints).map((x) => x?.id ?? 0);
+
+    const biggestId = existingIds.reduce(
+      (previous, current) => (current > previous ? current : previous),
+      0
+    );
+
+    return biggestId + 1;
+  }
+
+  public async remove(id: EndpointId): Promise<Endpoint | undefined> {
+    return await mutex.runExclusive(() => {
+      const found = this.endpoints[id];
+
+      if (found != null) {
+        this.endpoints[id] = undefined;
+      }
+
+      return found;
+    });
+  }
+
+  public getAll(): Endpoint[] {
+    return Object.values(this.endpoints).flatMap((x) => (x ? [x] : []));
+  }
 }
